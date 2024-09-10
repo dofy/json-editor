398be77940cfbd415839816e5bee96fea746d2d2
Seven Yu <dofyyu@gmail.com>
2024-09-11 03:20:23 +0800

    update newtab

--- src/pages/newtab/Newtab.tsx
+++ src/pages/newtab/Newtab.tsx
@@ -1,19 +1,23 @@
 import "@pages/newtab/Newtab.css";
+import pkg from "../../../package.json";
 
 export default function Newtab(): JSX.Element {
   return (
     <main className="w-screen h-screen flex flex-col justify-center items-center">
-      <div className="text-4xl text-blue-300 font-black">JSON Editor</div>
+      <div className="text-5xl text-blue-400">JSON Editor</div>
       <pre>
-        <code className="text-2xl">
+        <code className="text-2xl text-gray-500">
           {`
 {
   "name": "JSON Editor",
-  "version": "1.0.0",
-  "description": "A JSON editor extension",
-  "author": "Seven Yu <dofyyu@gmail.com>",
+  "version": "${pkg.version}",
+  "description": "A JSON Editor Extension for Chrome",
+  "author": {
+    "name": "Seven Yu",
+    "email": "dofyyu@gmail.com"
+  }
 }
-          `}
+`}
         </code>
       </pre>
     </main>
